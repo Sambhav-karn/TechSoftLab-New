240ee10baff1f5a562fdcb4461e279dad3f56488
Sambhav Kumar <153364087+Sambhav-karn@users.noreply.github.com>
2025-05-04 00:11:01 +0530

    Create Services.jsx

---
+++ techsoft-lab/src/header pages/Services.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Progress from '../Image/Progress.jpg'
+
+export default function Services() {
+  return (
+    <div className='flex justify-center items-center'>
+      <img src={Progress} alt="" className='w-full'/>
+    </div>
+  )
+}
